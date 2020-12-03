08d4ab2ec24eca121a7321fad5c9e5bae1b9a0fe
IroTech <souravrawat1999@gmail.com>
2020-12-03 17:01:33 +0530

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const [state, setstate] = useState({
+    uri: require("./src/images/dice1.png"),
+  });
+
+  getRandom = () => {
+    return Math.floor(Math.random() * 6) + 1;
+  };
+
+  onclick = () => {
+    switch (getRandom()) {
+      case 1:
+        setstate({ uri: require("./src/images/dice1.png") });
+        break;
+      case 2:
+        setstate({ uri: require("./src/images/dice2.png") });
+        break;
+      case 3:
+        setstate({ uri: require("./src/images/dice3.png") });
+        break;
+      case 4:
+        setstate({ uri: require("./src/images/dice4.png") });
+        break;
+      case 5:
+        setstate({ uri: require("./src/images/dice5.png") });
+        break;
+      case 6:
+        setstate({ uri: require("./src/images/dice6.png") });
+        break;
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Image source={state.uri}/>
+        <TouchableOpacity onPress={onclick}>
+          <Text style={styles.button}>Roll Me!</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -14,8 +46,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor:"#0ABDE3",
+    marginTop:40,
+    paddingVertical:5,
+    paddingHorizontal:20,
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+    borderRadius: 10,
   },
 });
